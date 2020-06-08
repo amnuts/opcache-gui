4ad3443223bbad66b621f25a7ce101617f63f838
Andrew Collington <andy@amnuts.com>
2020-06-08 16:49:42 +0100

    Trim contents before outputting

--- build/build.php
+++ build/build.php
@@ -5,9 +5,9 @@ chdir(dirname(__DIR__));
 exec('npm run compile-jsx');
 exec('npm run compile-scss');
 
-$template = file_get_contents(__DIR__ . '/template.php');
-$jsOutput = file_get_contents(__DIR__ . '/interface.js');
-$cssOutput = file_get_contents(__DIR__ . '/interface.css');
+$template = trim(file_get_contents(__DIR__ . '/template.php'));
+$jsOutput = trim(file_get_contents(__DIR__ . '/interface.js'));
+$cssOutput = trim(file_get_contents(__DIR__ . '/interface.css'));
 
 $output = str_replace(['{{JS_OUTPUT}}', '{{CSS_OUTPUT}}'], [$jsOutput, $cssOutput], $template);
 file_put_contents(dirname(__DIR__) . '/index.php', $output);
