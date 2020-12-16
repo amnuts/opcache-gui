46cd39e9f13492ac6fbd201100a4c9dea060983c
Andrew Collington <andy@amnuts.com>
2020-12-16 10:39:52 +0000

    Bumping version and updating readme

--- build/build.php
+++ build/build.php
@@ -4,7 +4,7 @@
  * OPcache GUI - build script
  *
  * @author Andrew Collington, andy@amnuts.com
- * @version 3.2.0
+ * @version 3.2.1
  * @link https://github.com/amnuts/opcache-gui
  * @license MIT, http://acollington.mit-license.org/
  */
